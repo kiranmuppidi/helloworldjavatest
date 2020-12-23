6b9cfb9959ad4a8d6051eb74badb347cf8d889ba
kiranmuppidi <76123711+kiranmuppidi@users.noreply.github.com>
2020-12-22 20:52:27 -0600

    Update helloworldjavatest.java

--- helloworldjavatest.java
+++ helloworldjavatest.java
@@ -2,6 +2,6 @@ public class helloworldjavatest{
 
 	public static void main(String[] args){
 		
-		System.out.println("Hello World again - from git hub testing SCM");
+		System.out.println("Hello World again - from git hub testing SCM - Practice test");
 	}
 }
